fe3b37335c990ff11cb32860a8c6045ba4b7b779
Kamarov <153866697+kamarov-therussiantank@users.noreply.github.com>
2026-06-23 18:12:32 +0200

    fix: update favourites logic to ensure proper string handling

--- uiadditions&improvements/src/scripts/favouriting.js
+++ uiadditions&improvements/src/scripts/favouriting.js
@@ -1,4 +1,4 @@
-// Favouriting logic
+// Favourites System
 whenContentInitialized().then(() => {
     const KEY = "favourites";
     function getFavourites() {
@@ -8,9 +8,10 @@ whenContentInitialized().then(() => {
         localStorage.setItem(KEY, JSON.stringify(list));
     }
     function isFavourite(id) {
-        return getFavourites().includes(id);
+        return getFavourites().includes(String(id));
     }
     function addFavourite(id) {
+        id = String(id);
         const list = getFavourites();
         if (!list.includes(id)) {
             list.push(id);
@@ -18,59 +19,104 @@ whenContentInitialized().then(() => {
         }
     }
     function removeFavourite(id) {
-        const list = getFavourites().filter(x => x !== id);
+        id = String(id);
+        const list = getFavourites().filter(
+            x => x !== id
+        );
         setFavourites(list);
     }
     Loader.interceptFunction(TankTrouble.TankInfoBox, '_initialize', (original, ...args) => {
-        original(...args);
-        TankTrouble.TankInfoBox._updateFavouriteStatus = function () {};
-        TankTrouble.TankInfoBox.infoFavouriteStarOn.insertBefore(TankTrouble.TankInfoBox.infoGameJoin);
-        TankTrouble.TankInfoBox.infoFavouriteStarOff.insertBefore(TankTrouble.TankInfoBox.infoGameJoin);
-        TankTrouble.TankInfoBox.infoFavouriteStarOn.tooltipster({
-            position: 'left',
-            offsetX: 5
-        });
-        TankTrouble.TankInfoBox.infoFavouriteStarOff.tooltipster({
-            position: 'left',
-            offsetX: 5
-        });
-        TankTrouble.TankInfoBox.infoFavouriteStarOff
-            .off("mouseup.customFav")
-            .on("mouseup.customFav", function () {
-                if (!TankTrouble.TankInfoBox.showing) return;
-                addFavourite(TankTrouble.TankInfoBox.playerId);
-                TankTrouble.TankInfoBox.infoFavouriteStarOff.hide();
-                TankTrouble.TankInfoBox.infoFavouriteStarOn.show();
-            });
-        TankTrouble.TankInfoBox.infoFavouriteStarOn
-            .off("mouseup.customFav")
-            .on("mouseup.customFav", function () {
-                if (!TankTrouble.TankInfoBox.showing) return;
-                removeFavourite(TankTrouble.TankInfoBox.playerId);
-                TankTrouble.TankInfoBox.infoFavouriteStarOn.hide();
-                TankTrouble.TankInfoBox.infoFavouriteStarOff.show();
-            });
-    });
-    Loader.interceptFunction(TankTrouble.TankInfoBox, 'show', (original, ...args) => {
-        original(...args);
-        const [,, playerId] = args;
-            const blockingButtons = [
-            TankTrouble.TankInfoBox.infoAdminLookup,
-            TankTrouble.TankInfoBox.infoSignUp,
-            TankTrouble.TankInfoBox.infoAccount,
-            TankTrouble.TankInfoBox.infoAdmin,
-            TankTrouble.TankInfoBox.infoLogOut
-        ];
-        const anyBlockingVisible = blockingButtons.some(btn => btn.is(':visible'));
-        if (isFavourite(playerId)) {
-            TankTrouble.TankInfoBox.infoFavouriteStarOn.show();
-            TankTrouble.TankInfoBox.infoFavouriteStarOff.hide();
-        } else {
-            TankTrouble.TankInfoBox.infoFavouriteStarOn.hide();
-            TankTrouble.TankInfoBox.infoFavouriteStarOff.show();
+            original(...args);
+            TankTrouble.TankInfoBox._updateFavouriteStatus = function() {};
+            if (!TankTrouble.TankInfoBox.infoFavouriteStarOn.parent().length) {
+                TankTrouble.TankInfoBox.infoFavouriteStarOn.insertBefore(TankTrouble.TankInfoBox.infoGameJoin);
+            }
+            if (!TankTrouble.TankInfoBox.infoFavouriteStarOff.parent().length) {
+                TankTrouble.TankInfoBox.infoFavouriteStarOff.insertBefore(TankTrouble.TankInfoBox.infoGameJoin);
+            }
+            TankTrouble.TankInfoBox.infoFavouriteStarOn
+                .off(".customFav");
+            TankTrouble.TankInfoBox.infoFavouriteStarOff
+                .off(".customFav");
+            TankTrouble.TankInfoBox.infoFavouriteStarOff.on(
+                "mouseup.customFav",
+                function() {
+                    if (!TankTrouble.TankInfoBox.showing)
+                        return;
+                    addFavourite(TankTrouble.TankInfoBox.playerId);
+                    TankTrouble.TankInfoBox.infoFavouriteStarOff.hide();
+                    TankTrouble.TankInfoBox.infoFavouriteStarOn.show();
+                    if (TankTrouble.TankInfoBox.username) {
+                        TankTrouble.TankInfoBox.infoFavouriteStarOn.tooltipster(
+                            'content', 'Remove ' + TankTrouble.TankInfoBox.username + ' from favourites'
+                        );
+                    }
+                }
+            );
+            TankTrouble.TankInfoBox.infoFavouriteStarOn.on(
+                "mouseup.customFav",
+                function() {
+                    if (!TankTrouble.TankInfoBox.showing)
+                        return;
+                    removeFavourite(TankTrouble.TankInfoBox.playerId);
+                    TankTrouble.TankInfoBox.infoFavouriteStarOn.hide();
+                    TankTrouble.TankInfoBox.infoFavouriteStarOff.show();
+                    if (TankTrouble.TankInfoBox.username) {
+                        TankTrouble.TankInfoBox.infoFavouriteStarOff.tooltipster(
+                            'content', 'Add ' + TankTrouble.TankInfoBox.username + ' to favourites'
+                        );
+                    }
+                }
+            );
         }
-        if (anyBlockingVisible) {
-            TankTrouble.TankInfoBox.infoFavouriteStarOn.hide();
-            TankTrouble.TankInfoBox.infoFavouriteStarOff.hide();
-    }});
+    );
+    Loader.interceptFunction(TankTrouble.TankInfoBox, 'show', (original, ...args) => {
+            original(...args);
+            const playerId = String(args[2]);
+            const localPlayers =
+                Users.getAllPlayerIds()
+                .map(String);
+            if (localPlayers.includes(playerId)) {
+                TankTrouble.TankInfoBox.infoFavouriteStarOn.hide();
+                TankTrouble.TankInfoBox.infoFavouriteStarOff.hide();
+                return;
+            }
+            const blockingButtons = [
+                TankTrouble.TankInfoBox.infoAdminLookup,
+                TankTrouble.TankInfoBox.infoSignUp,
+                TankTrouble.TankInfoBox.infoAccount,
+                TankTrouble.TankInfoBox.infoAdmin,
+                TankTrouble.TankInfoBox.infoLogOut
+            ];
+            const anyBlockingVisible =
+                blockingButtons.some(
+                    btn => btn.is(':visible')
+                );
+            if (anyBlockingVisible) {
+                TankTrouble.TankInfoBox.infoFavouriteStarOn.hide();
+                TankTrouble.TankInfoBox.infoFavouriteStarOff.hide();
+                return;
+            }
+            if (isFavourite(playerId)) {
+                TankTrouble.TankInfoBox.infoFavouriteStarOn
+                    .show()
+                    .removeClass("disabled");
+                TankTrouble.TankInfoBox.infoFavouriteStarOff.hide();
+            } else {
+                TankTrouble.TankInfoBox.infoFavouriteStarOff
+                    .show()
+                    .removeClass("disabled");
+                TankTrouble.TankInfoBox.infoFavouriteStarOn.hide();
+            }
+            if (TankTrouble.TankInfoBox.username) {
+                TankTrouble.TankInfoBox.infoFavouriteStarOn.tooltipster(
+                    'content', 'Remove ' + TankTrouble.TankInfoBox.username + ' from favourites'
+                );
+                TankTrouble.TankInfoBox.infoFavouriteStarOff.tooltipster(
+                    'content', 'Add ' + TankTrouble.TankInfoBox.username + ' to favourites'
+                );
+            }
+        }
+    );
+
 });
